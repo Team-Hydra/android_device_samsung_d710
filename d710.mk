c030d7a59fd691b371ff6fe304d5581cb11e6cf3
Emery Mitchell <github@shufflejoy.us>
2014-07-02 01:42:47 -0500

    d710: cleanup
    Change-Id: I0ee25b55687d43af3223995500768af141654fe4

--- d710.mk
+++ d710.mk
@@ -12,20 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.i
 
-# device specific overlays
 DEVICE_PACKAGE_OVERLAYS := \
-	device/samsung/d710/overlay \
-	device/samsung/galaxys2-common/overlay
+    device/samsung/d710/overlay \
+    device/samsung/galaxys2-common/overlay
 
-# inherit common makefile
 $(call inherit-product, device/samsung/d710/device.mk)
 
-# d710 CDMA properties
-PRODUCT_PROPERTY_OVERRIDES := \
-	ro.com.google.clientidbase=android-sprint-us \
-	ro.cdma.home.operator.numeric=310120 \
-	ro.cdma.home.operator.alpha=Sprint \
-    net.cdma.pppd.user=user[SPACE]SprintNextel
-
-# assert
 TARGET_OTA_ASSERT_DEVICE := epic4gtouch,SPH-D710,d710,smdk4210,SPH-D710VMUB,SPH-D710BST
